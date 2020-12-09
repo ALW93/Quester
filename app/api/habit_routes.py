db0ecd690e16078735a24e18c1ce060db20b8d2b
alycialwang@gmail.com <alycialwang@gmail.com>
2020-12-08 23:59:50 -0500

    Query Checks for Week Range

--- app/api/habit_routes.py
+++ app/api/habit_routes.py
@@ -1,6 +1,11 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required
 from app.models import db, Habit, Habit_Category, Check
+from datetime import datetime, timedelta, date
+
+dt = datetime.strptime(str(date.today()), '%Y-%m-%d')
+start = dt - timedelta(days=dt.weekday())
+end = start + timedelta(days=6)
 
 habit_routes = Blueprint("habits", __name__)
 
@@ -9,7 +14,9 @@ habit_routes = Blueprint("habits", __name__)
 @login_required
 def habit_checks(id):
     """Get Checks for a Habit"""
-    data = Check.query.filter(Check.habit_id == id).all()
+    dates = request.json
+    data = Check.query.filter(Check.habit_id == id) \
+        .filter(Check.date >= start).filter(Check.date <= end).all()
     checks = [check.to_dict() for check in data]
     check_json = jsonify({'checks': checks})
     return check_json
