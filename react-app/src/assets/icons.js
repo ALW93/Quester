3657fd4a012e7facc272a655752dc6b8c5b6b95f
alycialwang@gmail.com <alycialwang@gmail.com>
2020-12-11 14:16:07 -0500

    Mail System Coming Alive

--- react-app/src/assets/icons.js
+++ react-app/src/assets/icons.js
@@ -2,7 +2,9 @@ import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
 import EditIcon from "@material-ui/icons/Edit";
 import React from "react";
 import mail from "./letter_icon.svg";
+import potion from "./potion_icon.svg";
 
 export const deleteIcon = () => <DeleteOutlineIcon />;
 export const editIcon = () => <EditIcon />;
 export const mailIcon = () => <img src={mail} />;
+export const potionIcon = () => <img src={potion} style={{ width: "47px" }} />;
