56d53ec1896fc6b6329693deeac38e38c2018c48
alycialwang@gmail.com <alycialwang@gmail.com>
2020-12-11 21:13:46 -0500

    Good Work Bubble

--- react-app/src/User/Stats.js
+++ react-app/src/User/Stats.js
@@ -1,37 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import "./User.css";
 import { parseStatLevel } from "../services/levels";
-import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 
 const Stats = () => {
   const stats = useSelector((state) => state.stats.stats);
-  const update = useSelector((state) => state.utility.update);
-  const [showUpdate, hideShowUpdate] = useState(false);
-
-  useEffect(() => {
-    const statChange = () => {
-      setTimeout(() => hideShowUpdate(false), 10000);
-    };
-
-    if (update) {
-      console.log("stats updating");
-      hideShowUpdate(true);
-      statChange();
-    }
-  }, [update]);
 
   return (
     <>
       <h1>Stats</h1>
-      {showUpdate ? (
-        <>
-          <div className="test">
-            Level up !
-            <KeyboardArrowUpIcon />
-          </div>
-        </>
-      ) : null}
+
       {stats &&
         stats.map((stat, i) => {
           return (
