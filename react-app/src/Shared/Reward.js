26b947212265b465b7e94f489c064b991d59e88a
alycialwang@gmail.com <alycialwang@gmail.com>
2020-12-13 16:12:18 -0500

    Reward Migrated

--- react-app/src/Shared/Reward.js
+++ react-app/src/Shared/Reward.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { coinIcon, tempAvatar, expIcon, healthIcon } from "../assets/icons";
+import { coinIcon, expIcon, healthIcon } from "../assets/icons";
 import "./Reward.css";
 
 const Reward = ({ rewards, showReward }) => {
   return (
     <>
       <div className="reward__notice">
-        <h1>Rewards</h1>
-        <div>
-          {expIcon()} {rewards.exp} EXP
+        <div className="reward__inner">
+          <h1>Rewards</h1>
+          <div className="reward__detail">
+            <div>
+              {expIcon()} {rewards.exp} EXP
+            </div>
+            <div>
+              {coinIcon()} {rewards.currency} Gold
+            </div>
+            <div>
+              {healthIcon()} {rewards.health} HP
+            </div>
+          </div>
+          <button onClick={() => showReward(false)}>Collect</button>
         </div>
-        <div>
-          {coinIcon()} {rewards.currency} Gold
-        </div>
-        <div>
-          {healthIcon()} {rewards.health} HP
-        </div>
-        <button onClick={() => showReward(false)}>Collect</button>
       </div>
     </>
   );
