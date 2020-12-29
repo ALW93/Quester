ca322492f9bac80f7dae06e98ace34594d2972fd
alycialwang@gmail.com <alycialwang@gmail.com>
2020-12-28 20:02:43 -0500

    Refactor SignUp

--- react-app/src/Login/SignUpForm.js
+++ react-app/src/Login/SignUpForm.js
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
+import CreateAvatar from "../User/CreateAvatar";
 import { useDispatch, useSelector } from "react-redux";
 import { setAuth, setId, setUser, signUp } from "../store/actions/authReducer";
+import { createAvatar } from "../store/actions/avatarReducer";
 import { TextField, Button } from "@material-ui/core";
 import DoubleArrowIcon from "@material-ui/icons/DoubleArrow";
+import { setAvatar, getAvatar } from "../store/actions/avatarReducer";
+import { setUserInfo, authenticate } from "../store/actions/authReducer";
+import {
+  getTasks,
+  getComplete,
+  getExpired,
+} from "../store/actions/tasksReducer";
+import { getCategories } from "../store/actions/categoryReducer";
+import { getHabits } from "../store/actions/habitReducer";
+import { getStats } from "../store/actions/statReducer";
+import { getUserFriends, getUserMessages } from "../store/actions/userReducer";
 
 const SignUpForm = ({ showSignup }) => {
   const [username, setUsername] = useState("");
@@ -14,21 +27,42 @@ const SignUpForm = ({ showSignup }) => {
   const dispatch = useDispatch();
   const authorized = useSelector((state) => state.session.auth);
   const [beta, setBeta] = useState(false);
+  const [prebuilt, setPrebuilt] = useState("");
+  const [creation, showCreation] = useState(false);
 
   const onSignUp = async (e) => {
     e.preventDefault();
     if (password === repeatPassword) {
       const user = await signUp(username, email, password);
       if (!user.errors) {
+        let data = { prebuilt: prebuilt };
+        await createAvatar(user.id, data);
         dispatch(setAuth(true));
         dispatch(setId(user.id));
         dispatch(setUser(user));
+        dispatch([
+          setUserInfo(),
+          getAvatar(user.id),
+          getTasks(user.id),
+          getExpired(user.id),
+          getComplete(user.id),
+          getCategories(user.id),
+          getHabits(user.id),
+          getStats(user.id),
+          getUserFriends(user.id),
+          getUserMessages(user.id),
+        ]);
       } else {
         setErrors(user.errors);
       }
     } else {
       setErrors(["Passwords do not match!"]);
     }
+  };
+
+  const redirectCreation = (e) => {
+    e.preventDefault();
+    showCreation(true);
   };
 
   const updateUsername = (e) => {
@@ -48,61 +82,66 @@ const SignUpForm = ({ showSignup }) => {
   };
 
   if (authorized) {
-    return <Redirect to="/create-avatar" />;
+    return <Redirect to="/" />;
   }
 
   return (
     <>
-      <h2>Create an Account</h2>
       {errors.map((error) => (
         <div>{error}</div>
       ))}
-
-      {beta ? (
-        <h1>Currently under Development, Login with Demo to Tour!</h1>
+      {creation ? (
+        <CreateAvatar
+          prebuilt={prebuilt}
+          setPrebuilt={setPrebuilt}
+          onSignUp={onSignUp}
+        />
       ) : (
-        <form onSubmit={onSignUp}>
-          <div>
-            <TextField
-              type="text"
-              name="username"
-              placeholder="Username"
-              onChange={updateUsername}
-              value={username}
-            />
-          </div>
-          <div>
-            <TextField
-              type="text"
-              name="email"
-              placeholder="Email"
-              onChange={updateEmail}
-              value={email}
-            />
-          </div>
-          <div>
-            <TextField
-              type="password"
-              name="password"
-              placeholder="Password"
-              onChange={updatePassword}
-              value={password}
-            />
-          </div>
-          <div>
-            <TextField
-              type="password"
-              name="repeat_password"
-              placeholder="Confirm Password"
-              onChange={updateRepeatPassword}
-              value={repeatPassword}
-              required={true}
-            />
-          </div>
-          <button type="submit" className="fadebutton">
-            Sign Up
-          </button>
-        </form>
+        <>
+          <h2>Create an Account</h2>
+          <form>
+            <div>
+              <TextField
+                type="text"
+                name="username"
+                placeholder="Username"
+                onChange={updateUsername}
+                value={username}
+              />
+            </div>
+            <div>
+              <TextField
+                type="text"
+                name="email"
+                placeholder="Email"
+                onChange={updateEmail}
+                value={email}
+              />
+            </div>
+            <div>
+              <TextField
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={updatePassword}
+                value={password}
+              />
+            </div>
+            <div>
+              <TextField
+                type="password"
+                name="repeat_password"
+                placeholder="Confirm Password"
+                onChange={updateRepeatPassword}
+                value={repeatPassword}
+                required={true}
+              />
+            </div>
+            <button className="fadebutton" onClick={redirectCreation}>
+              Continue
+            </button>
+          </form>
+        </>
       )}
       <div onClick={() => showSignup(false)}>
         Return to Login
--- react-app/src/User/CreateAvatar.js
+++ react-app/src/User/CreateAvatar.js
@@ -1,86 +1,25 @@
 import React, { useState } from "react";
 import { parseLevel } from "../services/levels";
-import { useSelector, useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
 import { Select, MenuItem, Button } from "@material-ui/core";
-import { setAvatar, createAvatar } from "../store/actions/avatarReducer";
+import "./CreateAvatar.css";
 
-const CreateAvatar = () => {
-  const info = useSelector((state) => state.session.user);
-  const [hair, setHair] = useState("");
-  const [face, setFace] = useState("");
-  const [body, setBody] = useState("");
-  const [prebuilt, setPrebuilt] = useState("");
-  const history = useHistory();
-  const dispatch = useDispatch();
-
-  const createChar = async (e) => {
-    e.preventDefault();
-    const new_char = {
-      userId: info.id,
-      prebuilt: prebuilt,
-    };
-    const res = await createAvatar(info.id, new_char);
-
-    if (res) {
-      dispatch(setAvatar(res));
-      history.push("/");
-    }
-  };
-
-  const updateHair = (e) => {
-    setHair(e.target.value);
-  };
-
+const CreateAvatar = ({ prebuilt, setPrebuilt, onSignUp }) => {
   const updatePrebuilt = (e) => {
     setPrebuilt(e.target.value);
   };
 
-  const updateFace = (e) => {
-    setFace(e.target.value);
-  };
-
-  const updateBody = (e) => {
-    setBody(e.target.value);
-  };
-
   return (
-    <>
-      <h1>Create Your Character</h1>
-      <div>
-        {info.username} lv.{parseLevel(info.exp)}
-        <div className="progress">
-          <div
-            className="progress-bar bg-success"
-            role="progressbar"
-            style={{ width: `${info.health}%` }}
-          ></div>
-        </div>
-      </div>
-      <form onSubmit={createChar}>
+    <div className="creation__container">
+      <h1>Select a Character</h1>
+      <form onSubmit={onSignUp}>
         <Select value={prebuilt} onChange={updatePrebuilt}>
           <MenuItem value="girl_1">girl</MenuItem>
           <MenuItem value="boy_1">boy</MenuItem>
           <MenuItem value="animal_1">animal</MenuItem>
         </Select>
-        {/* <Select value={hair} onChange={updateHair}>
-          <MenuItem value="hair_1">hair_1</MenuItem>
-          <MenuItem value="hair_2">hair_2</MenuItem>
-          <MenuItem value="hair_3">hair_3</MenuItem>
-        </Select>
-        <Select value={face} onChange={updateFace}>
-          <MenuItem value="face_1">face_1</MenuItem>
-          <MenuItem value="face_2">face_2</MenuItem>
-          <MenuItem value="face_3">face_3</MenuItem>
-        </Select>
-        <Select value={body} onChange={updateBody}>
-          <MenuItem value="body_1">body_1</MenuItem>
-          <MenuItem value="body_2">body_2</MenuItem>
-          <MenuItem value="body_3">body_3</MenuItem>
-        </Select> */}
         <Button type="submit">Create</Button>
       </form>
-    </>
+    </div>
   );
 };
 
